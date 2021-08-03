0607d78851d0baa32fe6eeec5a020f85ddcdf604
Rashed <islamrashedul892@gmail.com>
2021-08-04 01:24:58 +0600

    lab5 done

---
+++ Lab5  [AJAX]/my_profile.php
@@ -0,0 +1,12 @@
+<html>
+	<head></head>
+	<?php $name = "Sadman";?>
+	<body>
+		<h1 align="center"> My Profile </h1>
+		<table align="center">
+			<tr>
+				<td><?php echo $name;?></td>
+			</tr>
+		</table>
+	</body>
+</html>
